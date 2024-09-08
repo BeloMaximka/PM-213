8cb61bbeb65bcc847a7ac5a2f13df7f9f6da3bde
BeloMaximka <belomaxim@gmail.com>
2024-09-09 02:59:25 +0300

    Added more test cases to GivenIncorrectNumberFormat_WhenParsing_ShouldThrowFormatExceptionWithMessage,  updated RomanNumber.Parse()

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -1,49 +1,121 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace App
+namespace App;
+
+public record RomanNumber(int Value)
 {
-    public record RomanNumber(int Value)
-    {
-        private readonly int _value = Value;
-        public int Value => _value;
+    private readonly int _value = Value;
+    public int Value => _value;
 
-        public static RomanNumber Parse(String input)
+    public static RomanNumber Parse(string input)
+    {
+        int value = 0;
+        int prevDigit = 0;
+        int pos = input.Length;
+        List<string> errors = [];
+        foreach (char c in input.Reverse())
         {
-            int value = 0;
-            int prevDigit = 0;
-            foreach (char c in input.Reverse())
+            pos -= 1;
+            int digit;
+            try
             {
-                int digit = DigitValue(c.ToString());
-                if (digit >= prevDigit)
-                {
-                    value += digit;
-                }
-                else
-                {
-                    value -= digit;
-                }
-                prevDigit = digit;
+                digit = DigitValue(c.ToString());
             }
-            return new(value);
+            catch
+            {
+                errors.Add($"Invalid character '{c}' at index {pos}");
+                continue;
+            }
+            if (digit != 0 && (prevDigit / digit > 10 || ShouldBeReplacedByRightDigit(digit, prevDigit) || ShouldBeReplacedByNextDigit(prevDigit, digit)))
+            {
+                errors.Add($"Invalid order '{c}' before {input[pos + 1]} at index {pos} in \"{input}\"");
+            }
+
+            value += digit >= prevDigit ? digit : -digit;
+            prevDigit = digit;
         }
 
-        public static int DigitValue(String digit) => digit switch
+        if (errors.Count != 0)
         {
-            "N" => 0,
-            "I" => 1,
-            "V" => 5,
-            "X" => 10,
-            "L" => 50,
-            "C" => 100,
-            "D" => 500,
-            _ => 1000
-            // "M" => 1000,
-            // _ => throw new ArgumentException("Invalid Roman digit.")
-        };
+            throw new FormatException(string.Join("; ", errors));
+        }
+        return new(value);
     }
 
+    public static int DigitValue(string digit) => digit switch
+    {
+        "N" => 0,
+        "I" => 1,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000,
+        _ => throw new ArgumentException($"{nameof(DigitValue)}: {nameof(RomanNumber)} has invalid value of {digit}", nameof(digit))
+    };
+
+    public override string ToString()
+    {
+        if (_value == 0) return "N";
+        Dictionary<int, string> parts = new()
+        {
+            { 1000, "M" },
+            { 900, "CM" },
+            { 500, "D" },
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" },
+        };
+
+        int tempValue = _value;
+        StringBuilder sb = new StringBuilder();
+        while (tempValue > 0)
+        {
+            foreach (var pair in parts)
+            {
+                if(tempValue >= pair.Key)
+                {
+                    tempValue -= pair.Key;
+                    sb.Append(pair.Value);
+                    break;
+                }
+            }
+        }
+        return sb.ToString();
+    }
+
+    private static bool ShouldBeReplacedByRightDigit(int leftDigitValue, int rightDigitValue)
+    {
+        return rightDigitValue - leftDigitValue == leftDigitValue;
+    }
+
+    private static bool ShouldBeReplacedByNextDigit(int firstValue, int secondValue)
+    {
+        return firstValue == secondValue && firstValue * 2 == GetNextDigitValue(firstValue);
+    }
+
+    private static int GetNextDigitValue(int value)
+    {
+        int[] values = [1, 5, 10, 50, 100, 500, 1000];
+        if (value == values[^1]) return value;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (value == values[i])
+            {
+                return values[i + 1]; 
+            }
+        }
+
+        return values[0];
+    }
 }
--- Test/RomanNumberTest.cs
+++ Test/RomanNumberTest.cs
@@ -1,75 +1,112 @@
 using App;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
-namespace Test
+namespace Test;
+
+[TestClass]
+public class RomanNumberTest
 {
-    [TestClass]
-    public class RomanNumberTest
+    [TestMethod]
+    [DynamicData(nameof(ValidNumberPairTestCases))]
+    public void ParseTest(string romanNumber, int number)
     {
-        [TestMethod]
-        public void ParseTest()
-        {
-            Dictionary<String, int> testCases = new()
-            {
-                { "N",    0 },
-                { "I",    1 },
-                { "II",   2 },
-                { "III",  3 },
-                { "IIII", 4 },   // цим тестом ми дозволяємо неоптимальну форму числа
-                { "IV",   4 },
-                { "VI",   6 },
-                { "VII",  7 },
-                { "VIII", 8 },
-                { "IX",   9 },
-                { "D",    500 },
-                { "M",    1000 },
-                { "CM",   900 },
-                { "MC",   1100 },
-                { "MCM",  1900 },
-                { "MM",   2000 },
-            };
-            foreach (var testCase in testCases)
-            {
-                RomanNumber rn = RomanNumber.Parse(testCase.Key);
-                Assert.IsNotNull(rn);
-                Assert.AreEqual(
-                    testCase.Value, 
-                    rn.Value, 
-                    $"{testCase.Key} -> {testCase.Value}"
-                );
-            }
-        }
+        RomanNumber rn = RomanNumber.Parse(romanNumber);
+        Assert.IsNotNull(rn);
+        Assert.AreEqual(
+        number,
+            rn.Value,
+            $"{romanNumber} -> {number}"
+        );
+    }
 
-        [TestMethod]
-        public void DigitValueTest()
-        {
-            Dictionary<String, int> testCases = new()
-            {
-                {"N", 0 },
-                {"I", 1 },
-                {"V", 5 },
-                {"X", 10 },
-                {"L", 50 },
-                {"C", 100 },
-                {"D", 500 },
-                {"M", 1000 },
-            };
-            foreach (var testCase in testCases)
-            {
-                Assert.AreEqual(
-                    testCase.Value, 
-                    RomanNumber.DigitValue(testCase.Key),
-                    $"{testCase.Key} -> {testCase.Value}");
-            }
-        }
+    [TestMethod]
+    [DataRow("W", 'W', 0)]
+    [DataRow("Q", 'Q', 0)]
+    [DataRow("s", 's', 0)]
+    [DataRow("sX", 's', 0)]
+    [DataRow("Xd", 'd', 1)]
+    public void GivenIncorrectCharacter_WhenParsing_ShouldThrowFormatExceptionWithMessage(string input, char incorrectCharacter, int position)
+    {
+        var act = () => RomanNumber.Parse(input);
+        act.Should().Throw<FormatException>().WithMessage($"Invalid character '{incorrectCharacter}' at index {position}");
+    }
+
+    [TestMethod]
+    [DataRow("IM", 'I', 'M', 0)]
+    [DataRow("XIM", 'I', 'M', 1)]
+    [DataRow("IMX", 'I', 'M', 0)]
+    [DataRow("XMD", 'X', 'M', 0)]
+    [DataRow("XID", 'I', 'D', 1)]
+    [DataRow("VV", 'V', 'V', 0)]
+    [DataRow("LL", 'L', 'L', 0)]
+    [DataRow("LC", 'L', 'C', 0)]
+    [DataRow("VX", 'V', 'X', 0)]
+    public void GivenIncorrectNumberFormat_WhenParsing_ShouldThrowFormatExceptionWithMessage(string input, char incorrectCharacter, char correctCharacter, int position)
+    {
+        var act = () => RomanNumber.Parse(input);
+        act.Should().Throw<FormatException>().WithMessage($"Invalid order '{incorrectCharacter}' before {correctCharacter} at index {position} in \"{input}\"");
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(ValidDigitPairTestCases))]
+    public void DigitValueTest(string romanDigit, int expectedNumber)
+    {
+
+        RomanNumber.DigitValue(romanDigit).Should().Be(expectedNumber);
+        var act = () => RomanNumber.DigitValue("IncorrectTestValue");
+        var ex = act.Should().Throw<ArgumentException>().WithParameterName("digit").And;
+        ex.Message.Should().NotBeNullOrEmpty();
+        ex.Message.Should().Contain("'digit'");
+        ex.Message.Should().Contain(nameof(RomanNumber));
+        ex.Message.Should().Contain(nameof(RomanNumber.DigitValue));
+        ex.Message.Should().Contain("IncorrectTestValue");
+    }
+
+    [TestMethod]
+    [DynamicData(nameof(ValidDigitPairTestCases))]
+    [DataRow("CXXIII", 123)]
+    [DataRow("CDXLIV", 444)]
+    [DataRow("CMXCIX", 999)]
+    [DataRow("MMMCCCXLIII", 3343)]
+    public void GivenSomeNumber_WhenCallingToString_ShouldReturnRomanNumberString(string expected, int number)
+    {
+        string result = new RomanNumber(number).ToString();
+        result.Should().Be(expected);
+    }
+
+    private static IEnumerable<object[]> ValidNumberPairTestCases
+    {
+        get => [
+                ["N",    0],
+                ["I",    1],
+                ["II",   2],
+                ["III",  3],
+                ["IIII", 4],   // цим тестом ми дозволяємо неоптимальну форму числа
+                ["IV",   4],
+                ["VI",   6],
+                ["VII",  7],
+                ["VIII", 8],
+                ["IX",   9],
+                ["D",    500],
+                ["M",    1000],
+                ["CM",   900],
+                ["MC",   1100],
+                ["MCM",  1900],
+                ["MM",   2000],
+        ];
+    }
+    private static IEnumerable<object[]> ValidDigitPairTestCases
+    {
+        get => [
+            ["N", 0 ],
+            ["I", 1 ],
+            ["V", 5 ],
+            ["X", 10 ],
+            ["L", 50 ],
+            ["C", 100 ],
+            ["D", 500 ],
+            ["M", 1000 ],
+        ];
     }
 }
-/* Д.З. Збільшити кількість тестових кейсів для ParseTest
- * Використовувати як оптимальні, так і неоптимальні форми чисел.
- * Перевірити працездатність шляхом включення неправильних кейсів
- * 
- * 
- * 
- * 
- * 
- * 
- */
